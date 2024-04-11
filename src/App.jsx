473b613ef01409fdcfea8ef51d2b6c03d232b282
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-04-11 21:36:15 +0530

    excel download added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { onSnapshot, collection } from "firebase/firestore";
 import "./App.css";
 import Graph from "./components/Graph";
 import Constituency from "./assets/ConstituencyData";
+import Button from "@mui/material/Button";
+import * as XLSX from "xlsx";
+
 export default function Home() {
   const [data, setData] = useState([]);
   const [stats, setStats] = useState({});
@@ -12,7 +15,7 @@ export default function Home() {
   const [levelFilter, setLevelFilter] = useState("");
   const [TotalLevels, setTotalLevels] = useState(0); //used in filtering
 
-  useEffect(() => { //fetching data
+  useEffect(() => {
     const unsubscribe = onSnapshot(collection(app, "users"), (snapshot) => {
       const userData = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -34,9 +37,9 @@ export default function Home() {
     return () => unsubscribe();
   }, []);
 
+  // Function to count the true values in the level array
   function countTrue(booleanArray) {
-    // Function to count the true values in the level array
-    if (!booleanArray) return 0; // Handle case where booleanArray is null or undefined
+    if (!booleanArray) return 0;
     let count = 0;
     for (let i = 0; i < booleanArray.length; i++) {
       if (booleanArray[i] === true) {
@@ -49,20 +52,19 @@ export default function Home() {
   useEffect(() => {
     let numberOfLevels = 0;
     data.forEach((user) => {
-      // loop to find the max no of levels
       if (user.level && user.level.length > numberOfLevels) {
         numberOfLevels = user.level.length;
         setTotalLevels(numberOfLevels);
       }
     });
-    // Setting the stat count dynamically based on the number of levels(l1,l2,l3,...);l1=lev 1 completed user's count
+
     const statCount = {};
     for (let i = 1; i <= numberOfLevels; i++) {
       statCount[`l${i}`] = 0;
     }
 
     data.forEach((user) => {
-      const levelsCompleted = user.level || Array(numberOfLevels).fill(false); // Fill with false if user.level is null or undefined
+      const levelsCompleted = user.level || Array(numberOfLevels).fill(false);
       levelsCompleted.forEach((level, index) => {
         if (level) {
           statCount[`l${index + 1}`]++;
@@ -74,9 +76,8 @@ export default function Home() {
   }, [data]);
 
   function convertToPercentage(obj) {
-    // Convert data in stats object to percentage
     const newObj = {};
-    const totalCount = data.length; // Total number of users
+    const totalCount = data.length;
     for (const key in obj) {
       if (Object.hasOwnProperty.call(obj, key)) {
         newObj[key] = ((obj[key] / totalCount) * 100).toFixed();
@@ -84,6 +85,40 @@ export default function Home() {
     }
     return newObj;
   }
+
+  //function to generate excel file
+  const exportToExcel = () => {
+    const fileName = "PresidingOfficersData.xlsx";
+    const worksheetData = data.map((user) => {
+      const { id, ...userData } = user; // Exclude id
+      const levelsCompleted = countTrue(user.level);
+      return {
+        Rank: user.index,
+        Name: user.Name,
+        Email: user.email,
+        Phone: user.Phone,
+        Constituency: user.Constituency,
+        "Levels Completed": levelsCompleted,
+      };
+    });
+    const worksheet = XLSX.utils.json_to_sheet(worksheetData);
+    const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    saveAsExcelFile(excelBuffer, fileName);
+  };
+  //function for excel file download button
+  const saveAsExcelFile = (buffer, fileName) => {
+    const data = new Blob([buffer], { type: "application/octet-stream" });
+    const url = window.URL.createObjectURL(data);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
 
   return (
     <>
@@ -110,7 +145,7 @@ export default function Home() {
                   <div className="search-container">
                     <input
                       type="text"
-                      placeholder="search user"
+                      placeholder="Search user"
                       onChange={(e) => {
                         setSearchKey(e.target.value.toLowerCase());
                       }}
@@ -124,18 +159,14 @@ export default function Home() {
                         onChange={(e) => setConstituencyKey(e.target.value)}
                       >
                         <option>All</option>
-                        {Constituency.map(
-                          (
-                            constituency //data from constituency.js file
-                          ) => (
-                            <option
-                              key={constituency.id}
-                              value={constituency.name}
-                            >
-                              {constituency.name}
-                            </option>
-                          )
-                        )}
+                        {Constituency.map((constituency) => (
+                          <option
+                            key={constituency.id}
+                            value={constituency.name}
+                          >
+                            {constituency.name}
+                          </option>
+                        ))}
                       </select>
                     </div>
                     <div>
@@ -152,12 +183,14 @@ export default function Home() {
                         <option value="100">100% </option>
                       </select>
                     </div>
+                    <Button variant="contained" onClick={exportToExcel} className="excel-btn" style={{ backgroundColor: '#04b904',fontSize:'12px',}}>
+                    Download as Excel
+                    </Button>
                   </div>
                 </td>
               </tr>
               <tr className="table-header">
                 <th>Rank</th>
-                {/* <th>Id</th> */}
                 <th className="tname">Name</th>
                 <th>Email</th>
                 <th>Phone</th>
@@ -165,27 +198,30 @@ export default function Home() {
                 <th>Levels Completed</th>
               </tr>
               {data
-                .filter((user) =>  //constituency filter
+                .filter((user) =>
                   constituencyKey === "All"
                     ? true
                     : user.Constituency &&
                       user.Constituency.includes(constituencyKey)
                 )
-                .filter(         //searching with id,name,email
+                .filter(
                   (user) =>
                     (user.Name &&
                       user.Name.toLowerCase().includes(searchKey)) ||
                     user.email.includes(searchKey)
                 )
-                .filter((user) => {  //level filter
+                .filter((user) => {
                   const completedLevels = countTrue(user.level);
                   if (levelFilter === "0") {
                     return completedLevels === 0;
                   } else if (levelFilter === "50") {
                     return completedLevels / TotalLevels < 0.5;
                   } else if (levelFilter === "50-100") {
-                    return completedLevels > 0.5 * TotalLevels && completedLevels <= TotalLevels;
-                  }else if (levelFilter === "100") {
+                    return (
+                      completedLevels > 0.5 * TotalLevels &&
+                      completedLevels <= TotalLevels
+                    );
+                  } else if (levelFilter === "100") {
                     return completedLevels === TotalLevels;
                   } else {
                     return true;
