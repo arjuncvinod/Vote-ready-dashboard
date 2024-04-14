69c57be3b70fe87bfc48a63a66f8d26fc0d1907b
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-04-14 23:18:59 +0530

    env added

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,23 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzz6h_UPTPC3SiIIbZMmc2b7_Xne9CLXo",
-  authDomain: "vote-ready-cb313.firebaseapp.com",
-  projectId: "vote-ready-cb313",
-  storageBucket: "vote-ready-cb313.appspot.com",
-  messagingSenderId: "915868952405",
-  appId: "1:915868952405:web:57397552c05c90aa04f1dc",
-  measurementId: "G-RSRF633GZ2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const app = getFirestore(firebaseApp);
-  
-  export { app };
+
+export { app };
